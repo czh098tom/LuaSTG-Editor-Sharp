77531fde6cd04bb25c41016caa41e9eaf838d4da
chenzihao <czh098tom@163.com>
2020-01-09 19:58:44 +0800

    Fixed minor bug

--- LuaSTGNodelib/EditorData/Node/CodeAlikeTypes.cs
+++ LuaSTGNodelib/EditorData/Node/CodeAlikeTypes.cs
@@ -12,9 +12,8 @@ namespace LuaSTGEditorSharp.EditorData.Node
         private static readonly Type[] types =
             { typeof(Stage.Stage), typeof(Object.CallBackFunc), typeof(Bullet.BulletInit), typeof(Boss.BossBGLayerInit)
             , typeof(Boss.BossBGLayerFrame), typeof(Boss.BossBGLayerRender), typeof(Boss.BossSCStart), typeof(Boss.BossSCFinish)
-            , typeof(Laser.LaserInit), typeof(Laser.BentLaserInit), typeof(Data.Function), typeof(Object.ObjectDefine)
-            , typeof(Task.TaskDefine), typeof(Boss.BossInit), typeof(Render.OnRender), typeof(Boss.Dialog)
-            , typeof(Enemy.EnemyInit) };
+            , typeof(Laser.LaserInit), typeof(Laser.BentLaserInit), typeof(Data.Function), typeof(Task.TaskDefine)
+            , typeof(Boss.BossInit), typeof(Render.OnRender), typeof(Boss.Dialog), typeof(Enemy.EnemyInit) };
 
         public IEnumerator<Type> GetEnumerator()
         {
