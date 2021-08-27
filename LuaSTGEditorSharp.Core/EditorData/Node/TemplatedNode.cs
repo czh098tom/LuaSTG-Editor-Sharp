32f0c513b1177ef35b3228519fdbd9399239f630
czh098tom <czh098tom@163.com>
2021-08-27 22:09:46 +0800

    fixed bugs

---
+++ LuaSTGEditorSharp.Core/EditorData/Node/TemplatedNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuaSTGEditorSharp.EditorData.Node
+{
+    public class TemplatedNode : TreeNode
+    {
+        private Dictionary<string, string> storedProperties;
+
+        public override object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Tuple<int, TreeNode>> GetLines()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
