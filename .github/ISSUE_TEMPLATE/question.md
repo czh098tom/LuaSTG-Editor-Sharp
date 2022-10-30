99c2b48719e4eaa40be1c367bf378c89cd27fb74
BAKAOLC <ritsukage@qq.com>
2022-10-30 20:37:57 +0800

    fix cli project and add ci

---
+++ .github/ISSUE_TEMPLATE/question.md
@@ -0,0 +1,22 @@
+---
+name: 提问问题
+about: 发起一个提问
+title: ''
+labels: 'question'
+assignees: ''
+
+---
+
+<!-- 
+  感谢你来到这里,
+  在反馈前, 请确认你已经做了下面这些事情
+  - 对照 releases，相关问题未在近期更新中解决
+  - 搜索了已有的 issues 列表中没有相关的信息
+-->
+
+<!--
+
+如果你要咨询如何使用等问题, 请在 https://github.com/BAKAOLC/Ritsukage-Core/discussions/new 发起 Q&A 而不要在这里提问.
+如果你发现 Ritsukage-Core 存在一些问题且这些问题不能算是 bug, 请在这里详细描述.
+
+请在下一行开始描述你的问题-->
--- LuaSTGEditorSharp.Core.Cli/Properties/AssemblyInfo.cs
+++ LuaSTGEditorSharp.Core.Cli/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("LuaSTGPlusLib")]
+[assembly: AssemblyTitle("LuaSTGEditorSharp.Core.Cli")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LuaSTGPlusLib")]
+[assembly: AssemblyProduct("LuaSTGEditorSharp.Core.Cli")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
